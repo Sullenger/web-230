91bd9e9cee50a1f41e962eab0d346bc6d8b7a181
Jason Sullenger <jssullenger@my365.bellevue.edu>
2018-11-30 19:14:05 -0700

    adding hello-world.js

---
+++ week-1/hello-world.js
@@ -0,0 +1,10 @@
+
+
+console.log("Hello World, my name is Jason Sullenger");
+
+/*
+Title: Excercise 1.3 - Hello-World
+Author: Jason Sullenger
+Date: 30 November 2018
+Description: First JavaScript assignment. Hello World.
+*/
