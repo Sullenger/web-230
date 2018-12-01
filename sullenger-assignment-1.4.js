81a6852627b2f1aab53b8ea20b997489c5acbe07
Jason Sullenger <jssullenger@my365.bellevue.edu>
2018-11-30 19:48:03 -0700

    adding assignment 1.4.js

---
+++ sullenger-assignment-1.4.js
@@ -0,0 +1,42 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Jason";
+var lastName = "Sullenger";
+var age = 30;
+var street = "123 Main St"
+var city = "Nampa"
+var state = "Idaho"
+var zipCode = "83686"
+
+// output 
+console.log(firstName); 
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+// end program 
+
+
+/*
+Title: Assignment 1.4 - Primitive Types
+Author: Jason Sullenger
+Date: 30 November 2018
+Description: Logs basic information about author of assigment
+*/
+
