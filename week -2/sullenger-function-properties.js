e743511912785b1bf0f6c68d9c025f162c17e252
Jason Sullenger <jssullenger@my365.bellevue.edu>
2018-12-07 17:23:05 -0700

    adding sullenger-function-properties.js

---
+++ week -2/sullenger-function-properties.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  sullenger-function-properties.js
+; Author: Jason Sullenger
+; Date:   7 December 2018
+; Description: Defining functions and calling function properties
+;===========================================
+*/
+
+
+
+// myName Function defined
+function myName() {
+  return myName.jason + " " + myName.sully
+};
+
+// myName function properties
+myName.jason = "Jason";
+myName.sully = "Sullenger";
+
+
+// Header function called
+const header = require('./sullenger-header.js');
+console.log(header.display("Jason" , "Sullenger" , "Exercise 2.3"));
+
+// Testing console log of myName function
+console.log("\n" + "Hello " + myName() + "!");
